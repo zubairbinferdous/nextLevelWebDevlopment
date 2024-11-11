b4d736c43cc56df1db1963ff76b52bc81fdfae39
SKRJS <zubidev71@gmail.com>
2024-11-12 03:26:49 +0600

    ok

--- typescript/model-1/src/1.11.ts
+++ typescript/model-1/src/1.11.ts
@@ -2,13 +2,40 @@
   //
 
   const age: number = 12;
-  const ageBro: number = 30;
+  const ageBro: number = 50;
 
   if (age > 19) {
     console.log("hello world this is me ");
   } else {
     console.log("hello there is nothing happen ");
   }
+
+  const a = ageBro > 40 ? "good boy" : "not good";
+  console.log(a);
+
+  const res = null;
+  const rs1 = res ?? "guest";
+  console.log(rs1);
+
+  type User = {
+    name: string;
+    role: number;
+    address: {
+      per: string;
+      pse?: string;
+    };
+  };
+
+  const user: User = {
+    name: "zubi",
+    role: 12,
+    address: {
+      per: "54 fin",
+    },
+  };
+
+  const per = user?.address?.pse ?? "there is no address";
+  console.log(per);
 
   //
 }
